a04751161c176081390ece1da9c06e380a5ea40f
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-04-05 09:08:46 +0200

    Add AppState

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -1,37 +1,53 @@
 use crate::drivers::rgb_led::{RgbLed, RgbLedColor};
 use crate::platform::Platform;
 
+#[derive(Default)]
+pub struct AppState {
+    is_wifi_connected: bool,
+}
+
 pub struct App<'a> {
     led_controller: LedController<'a>,
     platform: &'a Platform,
+    state: AppState,
 }
 
 impl<'a> App<'a> {
     pub fn new(platform: &'a mut Platform) -> Self {
         let led_controller = LedController {
             led: &platform.rgb_led,
-            is_wifi_connected: false,
         };
 
-        platform.wifi.start();
-
-        Self { led_controller, platform }
+        Self {
+            led_controller,
+            platform,
+            state: AppState::default(),
+        }
     }
 
     pub fn update(&mut self) {
-        self.led_controller.is_wifi_connected = self.platform.wifi.is_connected();
-        self.led_controller.update();
+        self.update_state();
+        self.led_controller.update(&self.state);
+    }
+
+    pub fn update_state(&mut self) {
+        let state = &mut self.state;
+        state.is_wifi_connected = self.platform.wifi.is_connected();
     }
 }
 
 struct LedController<'a> {
     led: &'a RgbLed,
-    is_wifi_connected: bool,
 }
 
 impl<'a> LedController<'a> {
-    pub fn update(&mut self) {
-        let color = if self.is_wifi_connected { 0x008000 } else { 0x800000 };
+    pub fn update(&mut self, app_state: &AppState) {
+        let color = if app_state.is_wifi_connected {
+            0x008000
+        } else {
+            0x800000
+        };
+
         self.led.set_color(RgbLedColor::from(color));
     }
 }
