ba6def25830c3de29ac4b4020730e3651304ab7d
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-04-06 07:57:33 +0200

    Refactor: only one implementation for gate, pin as dependency

--- src/drivers/gate.rs
+++ src/drivers/gate.rs
@@ -1,40 +1,20 @@
-use esp_idf_hal::gpio::{AnyInputPin, Input, InputPin, PinDriver, Pins};
+use esp_idf_hal::gpio::{AnyInputPin, Input, PinDriver};
 
 use crate::hal::gate::Gate;
 
-pub struct DevkitButton {
+pub struct EspGate {
     input: PinDriver<'static, AnyInputPin, Input>,
 }
 
-impl DevkitButton {
-    pub fn new(pins: Pins) -> Self {
-        let pin = pins.gpio9.downgrade_input();
+impl EspGate {
+    pub fn new(pin: AnyInputPin) -> Self {
         let input = PinDriver::input(pin).unwrap();
         let input = input.into_input().unwrap();
         Self { input }
     }
 }
 
-impl Gate for DevkitButton {
-    fn is_active(&self) -> bool {
-        self.input.is_low()
-    }
-}
-
-pub struct M5StampC3Button {
-    input: PinDriver<'static, AnyInputPin, Input>,
-}
-
-impl M5StampC3Button {
-    pub fn new(pins: Pins) -> Self {
-        let pin = pins.gpio3.downgrade_input();
-        let input = PinDriver::input(pin).unwrap();
-        let input = input.into_input().unwrap();
-        Self { input }
-    }
-}
-
-impl Gate for M5StampC3Button {
+impl Gate for EspGate {
     fn is_active(&self) -> bool {
         self.input.is_low()
     }
--- src/platform/m5stamp_c3.rs
+++ src/platform/m5stamp_c3.rs
@@ -1,7 +1,8 @@
+use esp_idf_hal::gpio::InputPin;
 use esp_idf_hal::peripherals::Peripherals;
 
 use crate::config::Config;
-use crate::drivers::gate::M5StampC3Button;
+use crate::drivers::gate::EspGate;
 use crate::drivers::rgb_led::WS2812RgbLed;
 use crate::drivers::wifi::EspWifi;
 use crate::hal::gate::Gate;
@@ -12,7 +13,7 @@ use crate::hal::Platform;
 pub struct PlatformImpl {
     pub wifi: EspWifi,
     pub rgb_led: WS2812RgbLed,
-    pub gate: M5StampC3Button,
+    pub gate: EspGate,
 }
 
 impl PlatformImpl {
@@ -21,7 +22,7 @@ impl PlatformImpl {
 
         let wifi = EspWifi::new(peripherals.modem, &config.wifi).expect("Cannot setup wifi");
         let rgb_led = WS2812RgbLed::default();
-        let gate = M5StampC3Button::new(peripherals.pins);
+        let gate = EspGate::new(peripherals.pins.gpio3.downgrade_input());
         Self {
             wifi,
             rgb_led,
