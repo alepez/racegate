6fccbfd3eb1b1c63ac3646059ce9021a5d937071
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-04-06 06:03:40 +0200

    Add M5Stamp platform implementation

---
+++ src/platform/m5stamp_c3.rs
@@ -0,0 +1,45 @@
+use esp_idf_hal::peripherals::Peripherals;
+
+use crate::config::Config;
+use crate::drivers::gate::M5StampC3Button;
+use crate::drivers::rgb_led::WS2812RgbLed;
+use crate::drivers::wifi::EspWifi;
+use crate::hal::gate::Gate;
+use crate::hal::rgb_led::RgbLed;
+use crate::hal::wifi::Wifi;
+use crate::hal::Platform;
+
+pub struct PlatformImpl {
+    pub wifi: EspWifi,
+    pub rgb_led: WS2812RgbLed,
+    pub gate: M5StampC3Button,
+}
+
+impl PlatformImpl {
+    pub fn new(config: &Config) -> Self {
+        let peripherals = Peripherals::take().unwrap();
+
+        let wifi = EspWifi::new(peripherals.modem, &config.wifi).expect("Cannot setup wifi");
+        let rgb_led = WS2812RgbLed::default();
+        let gate = M5StampC3Button::new(peripherals.pins);
+        Self {
+            wifi,
+            rgb_led,
+            gate,
+        }
+    }
+}
+
+impl Platform for PlatformImpl {
+    fn wifi(&self) -> &(dyn Wifi + '_) {
+        &self.wifi
+    }
+
+    fn rgb_led(&self) -> &(dyn RgbLed + '_) {
+        &self.rgb_led
+    }
+
+    fn gate(&self) -> &(dyn Gate + '_) {
+        &self.gate
+    }
+}
--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,8 +1,10 @@
-pub use rust_devkit::RustDevkit as PlatformImpl;
+// pub use rust_devkit::PlatformImpl;
+pub use m5stamp_c3::PlatformImpl;
 
 use crate::config::Config;
 use crate::hal::Platform;
 
+mod m5stamp_c3;
 mod rust_devkit;
 
 pub fn create(config: &Config) -> Box<dyn Platform> {
