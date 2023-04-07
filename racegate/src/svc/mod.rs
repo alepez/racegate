16f0de507effadd18ec2a4aff05b63f502ce86be
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-04-07 22:59:29 +0200

    Refactor: move EspRaceNode to racegate crate StdRaceNode

--- racegate/src/svc/mod.rs
+++ racegate/src/svc/mod.rs
@@ -1,9 +1,11 @@
 pub use race_node::RaceNode;
 pub use race_node::RaceNodeMessage;
+pub use std_race_node::StdRaceNode;
 
 use crate::app::SystemState;
 
 mod race_node;
+mod std_race_node;
 
 pub trait HttpServer {
     fn set_system_state(&self, status: &SystemState);
