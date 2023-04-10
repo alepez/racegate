49dedca8921dc80a2fd81b1ddfcffc09d983fa5e
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-04-10 12:04:04 +0200

    Add error handling

--- racegate/src/hal/wifi.rs
+++ racegate/src/hal/wifi.rs
@@ -11,20 +11,29 @@ pub struct WifiConfig<'a> {
     pub password: &'a str,
 }
 
+pub enum WifiConfigError {
+    EnvVarNotAvailable,
+    ParseError,
+}
+
 impl WifiConfig<'_> {
-    fn try_from_str(s: &'static str) -> Result<Self, ()> {
+    fn try_from_str(s: &'static str) -> Result<Self, WifiConfigError> {
         let mut iter = s.split_terminator(':');
-        let ap: bool = iter.next().ok_or(())?.parse().or(Err(()))?;
-        let ssid: &str = iter.next().ok_or(())?;
-        let password: &str = iter.next().ok_or(())?;
+        let ap: bool = iter
+            .next()
+            .ok_or(WifiConfigError::ParseError)?
+            .parse()
+            .or(Err(WifiConfigError::ParseError))?;
+        let ssid: &str = iter.next().ok_or(WifiConfigError::ParseError)?;
+        let password: &str = iter.next().ok_or(WifiConfigError::ParseError)?;
         Ok(WifiConfig { ap, ssid, password })
     }
 
-    pub fn from_env_var() -> Result<Self, ()> {
+    pub fn from_env_var() -> Result<Self, WifiConfigError> {
         if let Some(s) = option_env!("RACEGATE_WIFI_CONFIG") {
             WifiConfig::try_from_str(s)
         } else {
-            Err(())
+            Err(WifiConfigError::EnvVarNotAvailable)
         }
     }
 }
