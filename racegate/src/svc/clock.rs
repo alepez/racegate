ab153fd12f62c5e61b45464012de349e4a2317e7
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-04-11 12:00:27 +0200

    Fix clock integer overflow

--- racegate/src/svc/clock.rs
+++ racegate/src/svc/clock.rs
@@ -1,9 +1,13 @@
 #[derive(Default, Debug, Copy, Clone, Eq, PartialEq)]
-pub struct Instant(u16);
+pub struct Instant(u32);
 
 impl Instant {
-    pub fn from_millis(ms: u16) -> Self {
+    pub fn from_millis(ms: u32) -> Self {
         Self(ms)
+    }
+
+    pub fn to_millis(&self) -> u32 {
+        self.0
     }
 }
 
@@ -24,9 +28,9 @@ impl Clock {
         let t = std::time::Instant::now().checked_duration_since(self.start)?;
         let t_ms = t.as_millis();
 
-        // milliseconds, 16 bits, max 18 hours
-        if t_ms < (u16::MAX as u128) {
-            Some(Instant(t_ms as u16))
+        // milliseconds, 32 bits, max 49 days
+        if t_ms < (u32::MAX as u128) {
+            Some(Instant(t_ms as u32))
         } else {
             None
         }
